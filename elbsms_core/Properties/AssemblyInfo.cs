ab3b37374b91c0f848d6bab19648144a42a00c52
David Parrott <david.parrott@gmail.com>
2019-01-02 23:22:47 +0000

    Move a few more attributes to the common assembly info

--- elbsms_core/Properties/AssemblyInfo.cs
+++ elbsms_core/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("elbsms_core")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("elbsms_core")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -16,18 +15,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("59ef0f57-7a82-4cd8-80a6-0d0536ce1ed3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("elbsms_console")]
--- elbsms_ui/Properties/AssemblyInfo.cs
+++ elbsms_ui/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("elbsms_ui")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("elbsms_ui")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -15,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ed399644-5b1b-481c-b88c-eef053149ac0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
