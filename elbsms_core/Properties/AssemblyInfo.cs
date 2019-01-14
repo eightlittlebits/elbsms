487b60d45b367760c2c6c1e3a8d38c49314bb9d6
David Parrott <david.parrott@gmail.com>
2019-01-14 22:33:03 +0000

    Update assembly description attribute in elbsms_core

--- elbsms_core/Properties/AssemblyInfo.cs
+++ elbsms_core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("elbsms_core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("elbsms core")]
 [assembly: AssemblyProduct("elbsms_core")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
