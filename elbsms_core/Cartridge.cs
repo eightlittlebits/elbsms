1b4bc286e3f6ea9fad5b8ec8984cd851032d83c9
David Parrott <david.parrott@gmail.com>
2019-01-23 21:57:07 +0000

    Skip over a dumper/copier header, if present, when loading a rom

--- elbsms_core/Cartridge.cs
+++ elbsms_core/Cartridge.cs
@@ -8,7 +8,26 @@ namespace elbsms_core
 
         public static Cartridge LoadFromFile(string filename)
         {
-            byte[] fileData = File.ReadAllBytes(filename);
+            byte[] fileData;
+
+            using (FileStream file = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                // some roms have a 512 (0x200) byte header left by the dumper/copier. A normal ROM
+                // will have a length a multiple of 0x4000. If we mod with 0x4000 and have a 512 byte 
+                // remainder then skip the first 512 bytes of the rom.
+                if ((file.Length % 0x4000) == 0x200)
+                {
+                    fileData = new byte[file.Length - 0x200];
+                    file.Seek(0x200, SeekOrigin.Begin);
+                }
+                else
+                {
+                    /* Normal ROM */
+                    fileData = new byte[file.Length];
+                }
+
+                file.Read(fileData, 0, fileData.Length);
+            }
 
             return new Cartridge(fileData);
         }
