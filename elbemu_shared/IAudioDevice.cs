a373009d3ecc4b5f7d0360e02e8abd46156b9ce3
David Parrott <david.parrott@gmail.com>
2019-02-03 22:44:24 +0000

    Add audio device interface

---
+++ elbemu_shared/IAudioDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace elbemu_shared
+{
+    public interface IAudioDevice : IDisposable
+    {
+        void Initialise(IntPtr windowHandle);
+        void AddSample(short sample);
+        void RenderAudio();
+    }
+}
