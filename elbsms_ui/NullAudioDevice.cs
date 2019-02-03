b7183b599a2a2f26190a0fd20de2f52522f693f9
David Parrott <david.parrott@gmail.com>
2019-02-03 22:44:45 +0000

    Add a null audio device

---
+++ elbsms_ui/NullAudioDevice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using elbemu_shared;
+
+namespace elbsms_ui
+{
+    internal class NullAudioDevice : IAudioDevice
+    {
+        public void Initialise(IntPtr windowHandle)
+        {
+            
+        }
+
+        public void AddSample(short sample)
+        {
+            
+        }
+        
+        public void RenderAudio()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
