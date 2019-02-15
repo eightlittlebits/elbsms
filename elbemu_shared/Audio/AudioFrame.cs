f4b04e8a8d28dbf6eb850ab82e19c8086bb0fc41
David Parrott <david.parrott@gmail.com>
2019-02-15 10:32:41 +0000

    Add a concrete AudioFrame as generics are not currently considered unmanaged

--- elbemu_shared/Audio/AudioFrame.cs
+++ elbemu_shared/Audio/AudioFrame.cs
@@ -2,6 +2,7 @@
 
 namespace elbemu_shared.Audio
 {
+    // TODO(david): C# 8 will consider this unmanaged so can be used instead of AudioFrame
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public readonly struct AudioFrame<T> where T : unmanaged
     {
@@ -9,6 +10,19 @@ namespace elbemu_shared.Audio
         public readonly T Right;
 
         public AudioFrame(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public readonly struct AudioFrame
+    {
+        public readonly short Left;
+        public readonly short Right;
+
+        public AudioFrame(short left, short right)
         {
             Left = left;
             Right = right;
