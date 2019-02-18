016b3070e2b611c67a2e690f9631354077eea995
David Parrott <david.parrott@gmail.com>
2019-02-18 16:34:01 +0000

    Calculate memory control values on register write rather than on every read

--- elbsms_core/Memory/MemoryControlRegister.cs
+++ elbsms_core/Memory/MemoryControlRegister.cs
@@ -1,16 +1,29 @@
 ﻿namespace elbsms_core.Memory
 {
-    class MemoryControlRegister
+    internal class MemoryControlRegister
     {
         private byte _value;
 
-        public byte Value { set => _value = value; }
+        public bool ExpansionSlotEnabled;
+        public bool CartridgeSlotEnabled;
+        public bool CardSlotEnabled;
+        public bool WorkRamEnabled;
+        public bool BiosEnabled;
+        public bool IoChipEnabled;
 
-        public bool ExpansionSlotEnabled => (_value & 0x80) == 0;
-        public bool CartridgeSlotEnabled => (_value & 0x40) == 0;
-        public bool CardSlotEnabled => (_value & 0x20) == 0;
-        public bool WorkRamEnabled => (_value & 0x10) == 0;
-        public bool BiosEnabled => (_value & 0x08) == 0;
-        public bool IoChipEnabled => (_value & 0x04) == 0;
+        public byte Value
+        {
+            set
+            {
+                _value = value;
+
+                ExpansionSlotEnabled = (_value & 0x80) == 0;
+                CartridgeSlotEnabled = (_value & 0x40) == 0;
+                CardSlotEnabled = (_value & 0x20) == 0;
+                WorkRamEnabled = (_value & 0x10) == 0;
+                BiosEnabled = (_value & 0x08) == 0;
+                IoChipEnabled = (_value & 0x04) == 0;
+            }
+        }
     }
 }
