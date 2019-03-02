41d7a2910c52f81dae08463d80134c5732fa38f4
David Parrott <david.parrott@gmail.com>
2019-03-02 17:17:34 +0000

    Add an abstract ClockedComponent to handle synchronising components with the clock

---
+++ elbsms_core/ClockedComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace elbsms_core
+{
+    internal abstract class ClockedComponent
+    {
+        private SystemClock _clock;
+        private ulong _lastUpdate;
+
+        public ClockedComponent(SystemClock clock)
+        {
+            _clock = clock;
+        }
+
+        public void SynchroniseWithSystemClock()
+        {
+            ulong timestamp = _clock.Timestamp;
+            uint cyclesToUpdate = (uint)(timestamp - _lastUpdate);
+            _lastUpdate = timestamp;
+
+            Update(cyclesToUpdate);
+        }
+
+        // Run this component for the required number of cycles
+        public abstract void Update(uint cycleCount);
+    }
+}
