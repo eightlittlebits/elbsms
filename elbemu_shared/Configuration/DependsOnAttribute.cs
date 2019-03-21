045dfd1ddfaf96ed10e5cd779633b32ff407a8ef
David Parrott <david.parrott@gmail.com>
2019-03-21 16:55:02 +0000

    Tidy up DependsOnAttribute

--- elbemu_shared/Configuration/DependsOnAttribute.cs
+++ elbemu_shared/Configuration/DependsOnAttribute.cs
@@ -2,12 +2,11 @@
 
 namespace elbemu_shared.Configuration
 {
-    [AttributeUsage(System.AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-    public sealed class DependsOnAttribute : System.Attribute
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class DependsOnAttribute : Attribute
     {
         public string PropertyName { get; }
 
-        // This is a positional argument
         public DependsOnAttribute(string propertyName)
         {
             PropertyName = propertyName;
