6c09e3d08d37cac1046f2b57fb6b9db0e83dfbb3
David Parrott <david.parrott@gmail.com>
2019-06-15 22:08:29 +0100

    Handle loading of ROMs that have a copier header
    This was a regression when merging the ui branch

--- elbsms_console/Program.cs
+++ elbsms_console/Program.cs
@@ -75,31 +75,5 @@ namespace elbsms_console
 
             return $"{frequency:.##} {frequencyUnit[currentFreqUnit]}";
         }
-
-        private static byte[] LoadRomFromFile(string filename)
-        {
-            byte[] fileData;
-
-            using (FileStream file = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                // some roms have a 512 (0x200) byte header left by the dumper/copier. A normal ROM
-                // will have a length a multiple of 0x4000. If we mod with 0x4000 and have a 512 byte 
-                // remainder then skip the first 512 bytes of the rom.
-                if ((file.Length % 0x4000) == 0x200)
-                {
-                    fileData = new byte[file.Length - 0x200];
-                    file.Seek(0x200, SeekOrigin.Begin);
-                }
-                else
-                {
-                    /* Normal ROM */
-                    fileData = new byte[file.Length];
-                }
-
-                file.Read(fileData, 0, fileData.Length);
-            }
-
-            return fileData;
-        }
     }
 }
--- elbsms_core/GameMedia.cs
+++ elbsms_core/GameMedia.cs
@@ -17,7 +17,26 @@ namespace elbsms_core
 
         public static GameMedia LoadFromFile(string filename, GameMediaType mediaType)
         {
-            byte[] fileData = File.ReadAllBytes(filename);
+            byte[] fileData;
+
+            using (FileStream file = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                // some roms have a 512 (0x200) byte header left by the dumper/copier. A normal ROM
+                // will have a length a multiple of 0x4000. If we mod with 0x4000 and have a 512 byte 
+                // remainder then skip the first 512 bytes of the rom.
+                if ((file.Length % 0x4000) == 0x200)
+                {
+                    fileData = new byte[file.Length - 0x200];
+                    file.Seek(0x200, SeekOrigin.Begin);
+                }
+                else
+                {
+                    /* Normal ROM */
+                    fileData = new byte[file.Length];
+                }
+
+                file.Read(fileData, 0, fileData.Length);
+            }
 
             return new GameMedia(fileData, mediaType);
         }
